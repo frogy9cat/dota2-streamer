4c4be4e80549adbb02a6b8c14778c55e94119b02
frogy9cat <ramzeek_voker@mail.ru>
2023-09-29 10:51:40 +0500

    Big changes

--- config.py
+++ config.py
@@ -5,4 +5,13 @@ env = Env()
 env.read_env()
 
 BOT_TOKEN = env.str("BOT_TOKEN")
-CHANNELS = env.list("CHANNELS")
+
+ADMINS = env.str("ADMINS").split()
+CHANNELS = env.str("CHANNELS").split()
+VID_CHANNEL_ID = env.int("VID_CHANNEL_ID")
+VID_CHANNEL_TAG = env.str("VID_CHANNEL_TAG")
+
+API_ID = env.int("API_ID")
+API_HASH = env.str("API_HASH")
+
+OBS_PASSWORD = env.str("OBS_PASSWORD")
